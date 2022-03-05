d6a2bdb7a799fd17d1ea2b40337e2f5ab0c01876
MehtapAyall <mehtapayal60@gmail.com>
2022-03-05 21:01:20 +0300

    hhfhf

---
+++ resources/views/anasayfa.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/css/ozellik.css">
+    <title></title>
+  </head>
+  <body>
+<div class="üst_alan">
+<img src="resimler/logo.png"  width="150px" height="150px" alt="">
+<div class="menu"> @include('menu') </div>
+<div class="orta_alan">
+
+</div>
+  </body>
+</html>
