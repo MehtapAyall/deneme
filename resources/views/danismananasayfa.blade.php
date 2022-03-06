86d1d13b97920ffe76f02bdf51da94cba0bbe019
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-03-06 20:06:22 +0300

    zeyni

--- resources/views/danismananasayfa.blade.php
+++ resources/views/danismananasayfa.blade.php
@@ -7,9 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <link href="css/ilk.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="css/app.css">
     <title>Öğrenci Anasayfası</title>
   </head>
   <body>
@@ -23,13 +22,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto ">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Anasayfa</a>
+          <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
+        </li>
+            <li class="nav-item">
+          <a class="nav-link" href="#">Atanan Öğrenciler</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/ogrGiris">Öğrenci Giriş</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/dnmGiris">Danışman Giriş</a>
+          <a class="nav-link" href="#">Çıkış Yap</a>
         </li>
       </ul>
 
@@ -37,20 +36,9 @@
   </div>
 </nav>
     </header>
-
-    <section class="koyu_arkaplan" id="hero" style=" background: url({{url('/resimler/arka.jpg')}});" >
-      <div class="container">
-        <div class="d-flex h-100 flex-column text-light justify-content-center" >
-            
-            <table class="table table-dark table-striped">
-            <tr><td colspan="2" style="text-align:center;"><h1>Danışman Giriş Sayfası</h1></td></tr>
-            <tr><td style="text-align:right;"> <label for="">Öğrenci Numarası:</label></td> <th> <input type="text" > </th></tr>
-            <tr><td style="text-align:right;"> <label for="">Şifre:</label></td> <th> <input type="text" ></th></tr>
-            <tr><td style="text-align:right;"><button class="btn btn-danger">Kayıt Ol</button> </td><th><button class="btn btn-warning">Giriş Yap</button> </th></tr>                   
-            </table>
-        </div>
-      </div>
-    </section>
+    <section class="koyu_arkaplan" id="hero" style=" background: url({{url('/resimler/arka.jpg')}});">
+   
+        </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
