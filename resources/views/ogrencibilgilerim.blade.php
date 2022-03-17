beb4520389b4de7eea077801d8988c44fbe4a0bf
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-03-17 23:26:11 +0300

    yuggh

--- resources/views/ogrencibilgilerim.blade.php
+++ resources/views/ogrencibilgilerim.blade.php
@@ -22,13 +22,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto ">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
+          <a class="nav-link active" aria-current="page" href="/ogrAnasayfa">Anasayfa</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Danışman</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Bilgilerim</a>
+          <a class="nav-link" href="/ogrencibilgileri">Bilgilerim</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/">Çıkış Yap</a>
