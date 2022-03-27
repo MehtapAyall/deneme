4a082002c5112df5cfa3876b9a33b81bd43989ab
MehtapAyall <mehtapayal60@gmail.com>
2022-03-27 16:09:00 +0300

    sda

---
+++ database/migrations/2022_03_18_151213_create_danısman_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('danısman', function (Blueprint $table) {
+            $table->id('danisanID');
+            $table->string('ad');
+            $table->string('soyad');
+            $table->string('unvan');
+            $table->string('ePosta');
+            $table->string('sifre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('danısman');
+    }
+};
